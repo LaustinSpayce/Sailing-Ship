ca3e1d405ab0929a908d2853aa9ec59828c799c5
LaustinSpayce <LaustinSpayce@users.noreply.github.com>
2017-08-18 12:44:00 +0800

    Boat moves
    The boat now moves. Added a cute sail and everything. Put first video of project on facebook.

--- Sailing Ship/Assets/Scripts/BoatMover.cs
+++ Sailing Ship/Assets/Scripts/BoatMover.cs
@@ -4,13 +4,65 @@ using UnityEngine;
 
 public class BoatMover : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
+	public float m_MaxSpeed = 5.0f; // maximum speed
+	public float m_MaxTurnSpeed = 180f; // maximum turn rate in degrees per second
+	public float m_MinTurnSpeed = 10f; // minimum turn speed
+
+	private Rigidbody m_Rigidbody; // rigidbody for the boat.
+	private float m_CurrentSpeed; // current speed of the boat.
+	private float m_TargetSpeed; // Target Speed of boat.
+
+	void Awake ()
+	{
+		m_Rigidbody = GetComponent<Rigidbody>();
+	}
+
+	void Start () 
+	{
 		
 	}
 	
 	// Update is called once per frame
-	void Update () {
+	void Update () 
+	{
+		MoveBoat();
+		TurnBoat();
+	}
+
+	// Move the boat forward
+	void MoveBoat ()
+	{
+		GetTargetSpeed();
 		
+		Vector3 movement = transform.forward * m_TargetSpeed * Time.deltaTime;
+
+		m_Rigidbody.MovePosition(m_Rigidbody.position + movement);
+	}
+
+	// Speed of the boat going forward.
+	void GetTargetSpeed ()
+	{
+		if (Input.GetAxis("Vertical") >= 0) // The boat can only go forward so we ignore any reverse requests.
+			m_TargetSpeed = Input.GetAxis("Vertical") * m_MaxSpeed;
+		else m_TargetSpeed = 0; // if in reverse set the TargetSpeed to 0
+	}
+
+	// Turn the boat, the boat will only move the minimum when stationary, and the maximum at full speed.
+	void TurnBoat ()
+	{
+		// turnvalue from the input.
+		float turnValue = Input.GetAxis("Horizontal");
+
+		float turnMultiplier = Mathf.Lerp(m_MinTurnSpeed, m_MaxTurnSpeed, m_TargetSpeed / m_MaxSpeed);
+
+		float turn =  turnValue * turnMultiplier * Time.deltaTime;
+
+		// make it into a turn rotation on the y axis
+
+		Quaternion turnRotation = Quaternion.Euler (0f, turn, 0f);
+
+		// apply to rotation
+
+		m_Rigidbody.MoveRotation (m_Rigidbody.rotation * turnRotation);
 	}
 }
