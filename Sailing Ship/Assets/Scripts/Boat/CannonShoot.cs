237c54f49d79305d17a9df0bf088c6b54686ba35
LaustinSpayce <LaustinSpayce@users.noreply.github.com>
2017-08-18 17:32:08 +0800

    Simple Cannon Shot
    Added simple cannon shot. Plus some textures and palm trees to make the scene look a little nicer.

---
+++ Sailing Ship/Assets/Scripts/Boat/CannonShoot.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CannonShoot : MonoBehaviour {
+
+	public float m_ShotSpeed = 25.0f;
+	public Rigidbody m_CannonBall;
+	public float m_FiringAngle;
+	public Transform m_CannonTransform;
+
+	private bool m_Fired = false;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetButtonDown("Fire1") && m_Fired == false)
+			Fire();
+		else if (Input.GetButtonUp("Fire1") && m_Fired == true)
+			m_Fired = false;
+	}
+
+	void Fire ()
+	{
+		m_Fired = true;
+		Rigidbody cannonInstance = Instantiate (m_CannonBall, m_CannonTransform.position, m_CannonTransform.rotation) as Rigidbody;
+
+		cannonInstance.velocity = m_ShotSpeed * m_CannonTransform.forward;
+	}
+}
