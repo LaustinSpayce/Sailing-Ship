080034d9bb6d4ec00d8b8efc3c9c2192e36ace88
LaustinSpayce <LaustinSpayce@users.noreply.github.com>
2017-08-29 17:24:40 +0800

    Basic FMOD Implementation Achieved
    FMOD Implementation appears to be a success. Placeholder sounds added, further sounds to be implemented.

---
+++ Sailing Ship/Assets/Scripts/WaterCollider.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaterCollider : MonoBehaviour 
+{
+	[FMODUnity.EventRef]
+	public string m_CannonBallSplash = "event:/Input_1";
+
+	public float m_CannonBallSplashVelocity = 1.0f;
+
+	private Rigidbody m_CannonBallRigidBody;
+
+	void OnTriggerEnter (Collider Collider)
+	{
+		m_CannonBallRigidBody = Collider.gameObject.GetComponent<Rigidbody>(); // To calculate the velocity of the ball, hurrah.
+		if (Collider.gameObject.CompareTag("Projectile") && m_CannonBallRigidBody.velocity.magnitude > m_CannonBallSplashVelocity) // I don't want a splash if the cannon ball is slow.
+		{
+			var position = Collider.gameObject.transform.position;
+			Debug.Log("Splash!");
+			FMODUnity.RuntimeManager.PlayOneShot (m_CannonBallSplash, position); // Play splashing sound effect.
+			// Play a particle effect for the splash
+		}
+
+	}
+}
