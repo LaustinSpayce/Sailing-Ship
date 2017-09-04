14f28e27e3c2b73fb25fa18d72a9199e6256a592
Laustin Spayce <LaustinSpayce@users.noreply.github.com>
2017-09-04 13:35:47 +0800

    Meshes added to static geometry. Redid level
    Next step is to create a large "Target" Prefab - and add that throughout the level.

--- Sailing Ship/Assets/Scripts/WaterCollider.cs
+++ Sailing Ship/Assets/Scripts/WaterCollider.cs
@@ -19,7 +19,7 @@ public class WaterCollider : MonoBehaviour
 		if (Collider.gameObject.CompareTag("Projectile") && m_CannonBallRigidBody.velocity.magnitude > m_CannonBallSplashVelocity) // I don't want a splash if the cannon ball is slow.
 		{
 			var position = Collider.gameObject.transform.position;
-			Debug.Log("Splash!");
+			// Debug.Log("Splash!");
 			FMODUnity.RuntimeManager.PlayOneShot (m_CannonBallSplash, position); // Play splashing sound effect.
 			// Play a particle effect for the splash
 			Quaternion angle = Collider.gameObject.transform.rotation;
