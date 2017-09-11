04c9e13b295caa2770ad182803787b5a65bf60a0
Laustin Spayce <LaustinSpayce@users.noreply.github.com>
2017-09-11 12:55:12 +0800

    Added pause menu and some polish to GUI

--- Sailing Ship/Assets/Scripts/GameManager.cs
+++ Sailing Ship/Assets/Scripts/GameManager.cs
@@ -8,8 +8,10 @@ public class GameManager : MonoBehaviour
 	public static GameManager m_Instance = null;
 	public Text m_ScoreText;
 	public Rigidbody m_Gate;
+	public GameObject m_PausePanel;
 
 	private int m_Score = 0;
+	private bool m_Paused = false;
 
 	private void Awake()
 	{
@@ -21,12 +23,23 @@ public class GameManager : MonoBehaviour
 		{
 			Destroy(gameObject);
 		}
+		// if (m_PausePanel != null){
+		// 	m_PausePanel = GameObject.Find("PausePanel");
+		// }
 	}
 
 	private void Start ()
 	{
 		m_Score = 0;
 		UpdateScore();
+	}
+
+	private void Update()
+	{
+		if (Input.GetButtonDown("Cancel"))
+		{
+			PauseToggle();
+		}
 	}
 
 	public void DestroyedTarget()
@@ -44,8 +57,43 @@ public class GameManager : MonoBehaviour
 		}
 	}
 
+	public void PauseToggle()
+	{
+		if (m_Paused)
+		{
+			ResumeGame();
+		}
+		else if (!m_Paused)
+		{
+			PauseGame();
+		}
+	}
+
+	public void PauseGame()
+	{
+		Time.timeScale = 0.0f;
+		m_PausePanel.SetActive(true);
+		m_Paused = true;
+		Cursor.lockState = CursorLockMode.None;
+		Cursor.visible = true;
+	}
+
+	public void ResumeGame()
+	{
+		Time.timeScale = 1.0f;
+		m_PausePanel.SetActive(false);
+		m_Paused = false;
+		Cursor.lockState = CursorLockMode.Locked;
+		Cursor.visible = false;
+	}
+
 	private void OpenGate()
 	{
 
+	}
+
+	public void QuitGame()
+	{
+		Application.Quit();
 	}
 }
